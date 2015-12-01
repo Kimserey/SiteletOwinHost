d6a9df1a83ef4cca9101d5a3eb367e8f7ab49935
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2015-12-01 17:16:00 +0000

    Added Sitelet project and Owin host

---
+++ Host/Program.fs
@@ -0,0 +1,27 @@
+﻿module OwinHost
+    
+    open global.Owin
+    open Microsoft.Owin.Hosting
+    open Microsoft.Owin.StaticFiles
+    open Microsoft.Owin.FileSystems
+    open WebSharper.Owin
+    open MySitelet
+
+    [<EntryPoint>]
+    let Main = function
+        | [| rootDirectory; url |] ->
+                use server = WebApp.Start(url, fun appB ->
+                    let bin = @"C:\Projects\Host\MySitelet"
+                    
+                    appB.UseStaticFiles(StaticFileOptions(FileSystem = PhysicalFileSystem(bin))) |> ignore
+                    appB.UseSitelet(bin, MySitelet.Site.Main, binDirectory = bin) |> ignore
+                )
+                
+                stdout.WriteLine("Serving {0}", url)
+                stdin.ReadLine() |> ignore
+                0
+            | _ -> eprintfn "Failed to initialise sitelet"
+                   1
+        | _ ->
+            eprintfn "Usage: SiteletFsx ROOT_DIRECTORY URL"
+            1
